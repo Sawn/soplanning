1240694ecf7ae1f7236dcb7efa9400e4b6904344
OpenSVC <github@opensvc.com>
2016-01-05 20:28:28 +0100

    Clean pidfile & Launch apache binary instead of script

--- sh/run.sh
+++ sh/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-/etc/init.d/apache2 start
-/bin/bash
+for file in /run/apache2/apache2.pid /var/run/apache2/apache2.pid
+do
+    [[ -f $file ]] && {
+        /bin/rm -f $file && echo "Found $file. Deleted." 
+    }
+done
+
+/usr/sbin/apache2 -f /etc/apache2/apache2.conf -e info -DFOREGROUND
